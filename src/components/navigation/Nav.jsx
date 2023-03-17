61b0a7a0ce2db2237f652bca23744cbae32c3d21
Jeanne-Bsb <98830120+Jeanne-Bsb@users.noreply.github.com>
2023-03-17 16:52:44 +0100

    Header section en react

---
+++ src/components/navigation/Nav.jsx
@@ -0,0 +1,14 @@
+import { Logo } from "../logo/Logo"
+export const Nav = () => {
+    return (
+        <div>
+            <Logo/>
+            <li>LES FEMME PEINTRE</li>
+            <li>Acceuil</li>
+            <li>Exposition</li>
+            <li>Actualité</li>
+            <li>A propos</li>
+        </div>
+
+    )
+}
