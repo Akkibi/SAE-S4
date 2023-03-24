e4cd65b53ee1c7ab50ab1092f5a3b261ea2ddac5
nathanmno <115157657+nathanmno@users.noreply.github.com>
2023-03-24 16:09:58 +0100

    Modifs 24/03/2023

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { Login } from "./components/login";
-import { Inscription } from "./components/inscription";
 import { ReadAll } from "./components/Admin/readAll";
 import { Route, Routes } from "react-router-dom";
 import { Accueil } from "./components/Accueil/Accueil";
 import { Expo } from "./components/Expo/expositions";
 import { Equipe } from "./components/Equipe/equipe";
-import { Inscription } from "./components/FormUser/inscription";
+// import { Inscription } from "./components/FormUser/inscription";
+import PostForm from "./components/FormUser/connexion";
 
 const App = () => {
   return (
@@ -14,7 +13,8 @@ const App = () => {
       <Route path="/" element={<Accueil />} />
       <Route path="/expositions" element={<Expo />} />
       <Route path="/equipe" element={<Equipe />} />
-      <Route path="/incription" element={<Inscription />} />
+      {/* <Route path="/incription" element={<Inscription />} /> */}
+      <Route path="/test" element={<PostForm />} />
     </Routes>
   );
 };
